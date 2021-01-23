6c2cb533a3bd322ef3104a7eb1d989ed9a334e70
iamtreetop <trivta@gmail.com>
2021-01-22 23:44:15 -0800

    Add time and space complexity for Brute force and Hash solution

--- algo-expert/twoNumberSum.js
+++ algo-expert/twoNumberSum.js
@@ -1,24 +1,29 @@
 function twoNumberSum(array, targetSum) {
-    // brute force
-    // let i = 0, j = 0
-    // let sumsArr = [];
-    // for (let i = 0; i < array.length - 1; i++) {
-    // 	for (let j = i+1; j < array.length; j++) {
-    // 		if (array[i] + array[j] === targetSum) {
-    // 			sumsArr.push(array[i], array[j])
-    // 		}
-    // 	}
-    // }
-    // return sumsArr;
+  // Brute force
+  // Time: O(n^2)
+  // Space O(n)
+  // let i = 0, j = 0
+  // let sumsArr = [];
+  // for (let i = 0; i < array.length - 1; i++) {
+  // 	for (let j = i+1; j < array.length; j++) {
+  // 		if (array[i] + array[j] === targetSum) {
+  // 			sumsArr.push(array[i], array[j])
+  // 		}
+  // 	}
+  // }
+  // return sumsArr;
 
-    const numSet = new Set();
-    for (let num of array) {
-        const diff = targetSum - num;
-        if (numSet.has(diff)) {
-            return [diff, num];
-        } else {
-            numSet.add(num);
-        }
+  // Hash Table
+  // Time: O(n)
+  // Space: O(n)
+  const numSet = new Set();
+  for (let num of array) {
+    const diff = targetSum - num;
+    if (numSet.has(diff)) {
+        return [diff, num];
+    } else {
+        numSet.add(num);
     }
-    return [];
+  }
+  return [];
 }
