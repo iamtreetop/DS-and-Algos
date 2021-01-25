8e2751e518740a268b29503e5402bfddc34fc237
iamtreetop <trivta@gmail.com>
2021-01-25 13:27:38 -0800

    Add and solve bSearch iteratively, added time and space complexity

--- algo-expert/bSearch.js
+++ algo-expert/bSearch.js
@@ -1,4 +1,5 @@
-function binarySearch(array, target) {
+// O(log(n)) time | O(log(n)) space - where n length of input array
+function binarySearchRecur(array, target) {
   if (array.length === 0) return -1;
   
   const midpoint = Math.floor(array.length / 2);
@@ -12,3 +13,23 @@ function binarySearch(array, target) {
     return res === -1 ? -1 : res + midpoint + 1;
   } 
 };
+
+// O(log(n)) time | O(1) space - where n length of input array
+function binarySearchIter(array, target) {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left <= right) {
+    const midpoint = Math.floor((left + right) / 2);
+
+    if (array[midpoint] === target) {
+      return midpoint;
+    } else if (array[midpoint] < target) {
+      left = midpoint + 1;
+    } else {
+      right = midpoint - 1;
+    }
+  }
+  return -1;
+};
+
