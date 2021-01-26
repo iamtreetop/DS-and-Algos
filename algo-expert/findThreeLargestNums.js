9b12eb7c286d91dc3a7f517397718fd04c8ff840
iamtreetop <trivta@gmail.com>
2021-01-25 17:47:31 -0800

    Add and solve findThreeLargestNums

--- algo-expert/findThreeLargestNums.js
+++ algo-expert/findThreeLargestNums.js
@@ -8,13 +8,13 @@ function findThreeLargestNumbers(array) {
 };
 
 function updateLargest(threeLargest, num) {
-    if (threeLargest[2] === null || num > threeLargest[2]) {
-      shiftAndUpdate(threeLargest, num, 2);
-    } else if (threeLargest[1] === null || num > threeLargest[1]) {
-      shiftAndUpdate(threeLargest, num, 1);
-    } else if (threeLargest[0] === null || num > threeLargest[0]) {
-      shiftAndUpdate(threeLargest, num, 0);
-    }
+  if (threeLargest[2] === null || num > threeLargest[2]) {
+    shiftAndUpdate(threeLargest, num, 2);
+  } else if (threeLargest[1] === null || num > threeLargest[1]) {
+    shiftAndUpdate(threeLargest, num, 1);
+  } else if (threeLargest[0] === null || num > threeLargest[0]) {
+    shiftAndUpdate(threeLargest, num, 0);
+  }
 };
 
 function shiftAndUpdate(array, num, idx) {
