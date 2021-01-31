064675049618219a5ca4eb8165e005bf00d46ded
iamtreetop <trivta@gmail.com>
2021-01-30 16:45:32 -0800

    Add and solve leetcode 708

---
+++ leetcode/strings/easy/709.LowerCase.js
@@ -0,0 +1,21 @@
+// Implement function ToLowerCase() that has a string parameter str, and 
+// returns the same string in lowercase.
+
+var toLowerCase = function (str) {
+    return str.toLowerCase();
+};
+ 
+
+// Example 1:
+
+// Input: "Hello"
+// Output: "hello"
+// Example 2:
+
+// Input: "here"
+// Output: "here"
+// Example 3:
+
+// Input: "LOVELY"
+// Output: "lovely"
+
