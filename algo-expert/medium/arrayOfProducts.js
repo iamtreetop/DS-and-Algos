eed1f2f7a74f020daad506824bf662d3372b6a4b
iamtreetop <trivta@gmail.com>
2021-02-02 16:22:12 -0800

    Add and solve arrayOfProducts;2C

--- algo-expert/medium/arrayOfProducts.js
+++ algo-expert/medium/arrayOfProducts.js
@@ -1,3 +1,22 @@
+
+function arrayOfProducts(array) {
+  const products = new Array(array.length).fill(1);
+
+  let leftRunningProd = 1;
+  for (let i = 0; i < array.length; i++) {
+    products[i] = leftRunningProd;
+    leftRunningProd *= array[i];
+  }
+
+  let rightRunningProd = 1;
+  for (let i = array.length - 1; i >= 0; i--) {
+    products[i] *= rightRunningProd;
+    rightRunningProd *= array[i];
+  }
+
+  return products;
+};
+
 // Brute Force O(n^2) time | O(n^2) space
 function arrayOfProducts(array) {
   const products = [];
