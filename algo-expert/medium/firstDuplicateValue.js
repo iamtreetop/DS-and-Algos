68917b6f1bd1cef31a4dbe262d69a6925f95b9e7
iamtreetop <trivta@gmail.com>
2021-02-04 20:50:55 -0800

    Add notes to firstDupeValue problem

--- algo-expert/medium/firstDuplicateValue.js
+++ algo-expert/medium/firstDuplicateValue.js
@@ -1,5 +1,14 @@
 // O(n) time | O(n) space - n is length of input array
 function firstDuplicateValue3(array) {
+	// since all values are btwn 1 & n, they cna all be mapped to an index in the array
+		// cannot map n to nth index because oob, so n-1
+	// array = [5,1,3,2,5,3]
+	// idx = [0,1,2,3,4,5]
+	// walk over input array  
+		// map value to an index & make the value at that mapped index negative  
+		// ex. 5 maps to 4, see 5 for the seocnd time, it maps to index 4, index 4 is negative, therefore is a dupe
+		// if ele is already negative, return ele
+
 	for (let num of array) {
 		const absValue = Math.abs(num);
 		if (array[absValue - 1] < 0) {
@@ -12,6 +21,11 @@ function firstDuplicateValue3(array) {
 
 // O(n) time | O(n) space - n is length of input array
 function firstDuplicateValue2(array) {
+	// data structure to store already seen ele
+	// walk over input array
+		// if ele is not in data structure, add/push to the data structure
+		// if it is, return that ele
+	// return -1, if no dupes
 	const alreadySeen = new Set();
 	for (let num of array) {
 		if(alreadySeen.has(num)) {
