732a86cd3a8e3f16c4042d4506283dc753e7c27a
iamtreetop <trivta@gmail.com>
2021-02-05 18:02:46 -0800

    Optimize maxSubset problem and add notes

--- algo-expert/medium/onAdjSubsetSum.js
+++ algo-expert/medium/onAdjSubsetSum.js
@@ -1,6 +1,8 @@
 // time O(n) | space space O(n)
 function maxSubsetSumNoAdjacent(array) {
   // DP
+  // [75, 105, 120, 75, 90, 135]
+  // [75, 105, 195, 195, 285, 330]
   if (!array.length) return 0;
 	if (array.length === 1) return array[0];
 	
@@ -12,3 +14,25 @@ function maxSubsetSumNoAdjacent(array) {
 	}
 	return maxSums[maxSums.length - 1];
 };
+
+// time O(n) | space space O(n) - where n is length of input array 
+function maxSubsetSumNoAdjacent(array) {
+
+  // [75, 105, 120, 75, 90, 135]
+  // i = 5; 135
+  // current =  330
+  // second = 285
+  // first = 330
+
+  if (!array.length) return 0;
+	if (array.length === 1) return array[0];
+	
+	let second = array[0];
+	let first = Math.max(array[0], array[1])
+	for (let i = 2; i < array.length; i++) {
+		const current = Math.max(first, second + array[i]);
+		second = first;
+		first = current;
+	}
+	return first;
+}
