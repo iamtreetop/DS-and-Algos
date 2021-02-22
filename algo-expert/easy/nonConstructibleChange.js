9017ca98ed4d18546ddbb9a54cdb3da50a893049
iamtreetop <trivta@gmail.com>
2021-02-21 21:22:48 -0800

    Add and solve nonConstructibleChange

--- algo-expert/easy/nonConstructibleChange.js
+++ algo-expert/easy/nonConstructibleChange.js
@@ -1,7 +1,24 @@
 function nonConstructibleChange(coins) {
-  // Write your code here.
-  return 1;
-};
+  // sort the input array in place
+	// walk through array
+	// check value of coin
+		// if coin is >  current change, return current change + 1
+		// else add value of coin to current change
+	// return currne change + 1
+	
+	coins.sort((a, b) => a - b);
+	
+	let currentChange = 0;
+	for (const coin of coins) {
+		if (coin > currentChange + 1) return currentChange + 1;
+		currentChange += coin
+	}
+	
+  return currentChange + 1;
+}
 
-coins =  [5, 7, 1, 1, 2, 3, 22]
+coins = [5, 7, 1, 1, 2, 3, 22]
 // outpout: 20
+
+// coins = [1, 2, 5]
+// outpout: 4
