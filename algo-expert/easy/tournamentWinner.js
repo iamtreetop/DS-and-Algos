bd68e9c2970b1740878f342b34146b3b1e9cc2fa
iamtreetop <trivta@gmail.com>
2021-02-22 21:12:05 -0800

    Solve tournamentWinner algorithm

--- algo-expert/easy/tournamentWinner.js
+++ algo-expert/easy/tournamentWinner.js
@@ -1,6 +1,29 @@
-function tournamentWinner(competitions, results) {
+const HOME_TEAM_WON = 1;
 
-}
+function tournamentWinner(competitions, results) {
+  let currBestTeam = '';
+	const scores = {[currBestTeam] : 0}
+  
+	for (let i = 0; i < competitions.length; i++) {
+		const result = results[i];
+		const [homeTeam, awayTeam] = competitions[i];
+		
+		const winningTeam = result === HOME_TEAM_WON ? homeTeam : awayTeam;
+		
+		updateScores(winningTeam, 3, scores);
+		
+		if (scores[winningTeam] > scores[currBestTeam]) {
+			currBestTeam = winningTeam;
+		}
+	}
+	
+	return currBestTeam;
+};
+
+function updateScores(team, points, scores) {
+	if (!(team in scores)) scores[team] = 0;
+	scores[team] += points
+};
 
 competitions = [
   ["HTML", "C#"],
