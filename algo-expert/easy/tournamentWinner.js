0724a552003e34ad363862b971bcaa83a30c3551
iamtreetop <trivta@gmail.com>
2021-02-22 21:10:07 -0800

    Add tournamentWinner problem and example

---
+++ algo-expert/easy/tournamentWinner.js
@@ -0,0 +1,11 @@
+function tournamentWinner(competitions, results) {
+
+}
+
+competitions = [
+  ["HTML", "C#"],
+  ["C#", "Python"],
+  ["Python", "HTML"]
+]
+results = [0, 0, 1]
+//output: "Python"
