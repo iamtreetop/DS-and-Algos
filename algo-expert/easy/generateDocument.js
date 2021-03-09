c1b3f2de8f5431464dc12c3d8b6ffff6121cdb93
iamtreetop <trivta@gmail.com>
2021-03-08 16:24:44 -0800

    Add optimal solution to generate document

--- algo-expert/easy/generateDocument.js
+++ algo-expert/easy/generateDocument.js
@@ -1,15 +1,20 @@
+// basically anagrams question
+// time O(n+m) | space O(n) - where m and n are length of inputs, and c in hash
 function generateDocument(characters, document) {
-  const charCounts = {};
+	const counts = {};
 	
 	for (const char of characters) {
-		if(!(char in charCounts)) charCounts[char] = 0;
-		charCounts[char]++;
-	}
-	
+		if (!(char in counts)) counts[char] = 0;
+			counts[char]++;	
+		}
+	// console.log(counts)
 	for (const char of document) {
-		if(!(char in charCounts) || charCounts[char] === 0) return false;
-		
-		charCounts[char]--;
+		if (!(char in counts) || counts[char] === 0) return false;
+		counts[char]--;
 	}
-	return true
-}
+	return true;
+};
+
+// characters = "Bste!hetsi ogEAxpelrt x ";
+// document = "AlgoExpert is the Best!";
+// output = true
