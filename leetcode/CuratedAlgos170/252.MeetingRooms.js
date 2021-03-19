33a87e3255045620270709875de475c297d92f98
iamtreetop <trivta@gmail.com>
2021-03-18 19:49:22 -0700

    Clean up leetcode 252, add deconstruction

--- leetcode/CuratedAlgos170/252.MeetingRooms.js
+++ leetcode/CuratedAlgos170/252.MeetingRooms.js
@@ -1,13 +1,13 @@
 var canAttendMeetings = function(intervals) {
   // sort meeting times by start time
-  intervals.sort((a, b) => {
-    return a[0] - b[0];
-  });
-  
+  intervals.sort((a, b) => a[0] - b[0]);
+
   // check if any meeting times overlap (end i > start i+1)
-  for (let i = 0; i < intervals.length-1; i++)   
-    if (intervals[i][1] > intervals[i+1][0]) {
-      return false;
-    }
+  for (let i = 0; i < intervals.length - 1; i++) {
+    const [start1, end1] = intervals[i];
+    const [start2, end2] = intervals[i+1];
+    if(start2 < end1) return false;
+  }
+
   return true;
 };
