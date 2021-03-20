544654e0bc35d8059f6e6b20b89ea51d357034fc
iamtreetop <trivta@gmail.com>
2021-03-19 22:03:08 -0700

    Add and solve leetcode #1427

---
+++ leetcode/CuratedAlgos170/1427.PerformStringShifts.js
@@ -0,0 +1,17 @@
+var stringShift = function(s, shift) {
+  let chars = s.split('');
+  for(let i = 0; i < shift.length; i++) {
+    if(shift[i][0] === 0) {
+      while(shift[i][1] > 0) {
+        chars.push(chars.shift());
+        shift[i][1]--;
+      }
+    } else {
+      while(shift[i][1] > 0) {
+        chars.unshift(chars.pop());
+        shift[i][1]--;
+      }
+    }
+  }
+  return chars.join('');
+};
