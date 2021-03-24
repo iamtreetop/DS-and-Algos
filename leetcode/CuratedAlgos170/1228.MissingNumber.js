aa4168815c1049980258a77e130683f8e787e76a
iamtreetop <trivta@gmail.com>
2021-03-23 22:19:52 -0700

    Solve leetcode 1228

--- leetcode/CuratedAlgos170/1228.MissingNumber.js
+++ leetcode/CuratedAlgos170/1228.MissingNumber.js
@@ -6,7 +6,25 @@
 
 // Return the removed value.
 
- 
+var missingNumber = function(arr) {
+  let idx = 0, largestSub = 0, commonSub = 0, res = -1
+  for(let i=1; i<arr.length;i++){
+    const prev = arr[i-1], curr = arr[i]
+    const sub = Math.abs(prev-curr)
+    if(sub > largestSub){
+      largestSub = sub
+      idx = i
+    }else{
+      commonSub = sub
+    }
+  }  
+  if(arr[idx] > arr[idx-1]){
+    res = arr[idx] - commonSub //if ascending
+  }else{
+    res = arr[idx] + commonSub //if descending
+  }
+  return res
+};
 
 // Example 1:
 // Input: arr = [5,7,11,13]
