c1505842da96ca60f47acddcf1f83e66e631fa42
iamtreetop <trivta@gmail.com>
2021-03-26 10:37:42 -0700

    Solve leetcode 716 MaxStack

--- leetcode/CuratedAlgos170/716.MaxStack.js
+++ leetcode/CuratedAlgos170/716.MaxStack.js
@@ -1,44 +1,49 @@
-var MaxStack = function () {
-
+/**
+ * initialize your data structure here.
+ */
+var MaxStack = function() {
+  this.stack = [];
 };
 
-/**
+/** 
  * @param {number} x
  * @return {void}
  */
-MaxStack.prototype.push = function (x) {
-
+MaxStack.prototype.push = function(x) {
+  return this.stack.push(x)
 };
 
 /**
  * @return {number}
  */
-MaxStack.prototype.pop = function () {
-
+MaxStack.prototype.pop = function() {
+  return this.stack.pop()
 };
 
 /**
  * @return {number}
  */
-MaxStack.prototype.top = function () {
-
+MaxStack.prototype.top = function() {
+  return this.stack[this.stack.length - 1]
 };
 
 /**
  * @return {number}
  */
-MaxStack.prototype.peekMax = function () {
-
+MaxStack.prototype.peekMax = function() {
+  return Math.max(...this.stack)
 };
 
 /**
  * @return {number}
  */
-MaxStack.prototype.popMax = function () {
-  
+MaxStack.prototype.popMax = function() {
+  let max = Math.max(...this.stack);
+  let idx = this.stack.lastIndexOf(max);
+  return this.stack.splice(idx,1);
 };
 
-/**
+/** 
  * Your MaxStack object will be instantiated and called as such:
  * var obj = new MaxStack()
  * obj.push(x)
