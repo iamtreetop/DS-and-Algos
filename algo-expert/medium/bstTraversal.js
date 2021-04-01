888be9e75987a5229afb4083b6db1f1c4ede83e1
iamtreetop <trivta@gmail.com>
2021-04-01 11:20:48 -0700

    Add BST Traversal problem from AlgoExpert

---
+++ algo-expert/medium/bstTraversal.js
@@ -0,0 +1,24 @@
+function inOrderTraverse(tree, array) {
+  // Write your code here.
+}
+
+function preOrderTraverse(tree, array) {
+  // Write your code here.
+}
+
+function postOrderTraverse(tree, array) {
+  // Write your code here.
+}
+
+// {
+//   "nodes": [
+//     {"id": "10", "left": "5", "right": "15", "value": 10},
+//     {"id": "15", "left": null, "right": "22", "value": 15},
+//     {"id": "22", "left": null, "right": null, "value": 22},
+//     {"id": "5", "left": "2", "right": "5-2", "value": 5},
+//     {"id": "5-2", "left": null, "right": null, "value": 5},
+//     {"id": "2", "left": "1", "right": null, "value": 2},
+//     {"id": "1", "left": null, "right": null, "value": 1}
+//   ],
+//   "root": "10"
+// }
