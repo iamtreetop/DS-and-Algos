d075f88dc188654764aeb65b8b5f1e860066b613
iamtreetop <trivta@gmail.com>
2021-04-01 14:24:28 -0700

    Solve BST Traversal problem from AlgoExpert

--- algo-expert/medium/bstTraversal.js
+++ algo-expert/medium/bstTraversal.js
@@ -1,24 +1,49 @@
+// O(n) time - where n is a node
+// O(n) space - array stored, if not array, space would be O(d) 
 function inOrderTraverse(tree, array) {
-  // Write your code here.
+  if (tree !== null) {
+    inOrderTraverse(tree.left, array);
+		array.push(tree.value);
+		inOrderTraverse(tree.right, array);
+	}
+	return array
 }
 
+// O(n) time - where n is a node
+// O(n) space - array stored, if not array, space would be O(d) 
 function preOrderTraverse(tree, array) {
-  // Write your code here.
+  if (tree !== null) {
+    array.push(tree.value);
+		preOrderTraverse(tree.left, array);
+		preOrderTraverse(tree.right, array);
+	}
+	return array
 }
 
+// O(n) time - where n is a node
+// O(n) space - array stored, if not array, space would be O(d) 
 function postOrderTraverse(tree, array) {
-  // Write your code here.
+  if (tree !== null) {
+		postOrderTraverse(tree.left, array);
+		postOrderTraverse(tree.right, array);
+		array.push(tree.value);
+	}
+	return array
 }
+
+// "nodes": [
+//   {"id": "10", "left": "5", "right": "15", "value": 10},
+//   {"id": "15", "left": null, "right": "22", "value": 15},
+//   {"id": "22", "left": null, "right": null, "value": 22},
+//   {"id": "5", "left": "2", "right": "5-2", "value": 5},
+//   {"id": "5-2", "left": null, "right": null, "value": 5},
+//   {"id": "2", "left": "1", "right": null, "value": 2},
+//   {"id": "1", "left": null, "right": null, "value": 1}
+// ],
+// "root": "10"
 
 // {
-//   "nodes": [
-//     {"id": "10", "left": "5", "right": "15", "value": 10},
-//     {"id": "15", "left": null, "right": "22", "value": 15},
-//     {"id": "22", "left": null, "right": null, "value": 22},
-//     {"id": "5", "left": "2", "right": "5-2", "value": 5},
-//     {"id": "5-2", "left": null, "right": null, "value": 5},
-//     {"id": "2", "left": "1", "right": null, "value": 2},
-//     {"id": "1", "left": null, "right": null, "value": 1}
-//   ],
-//   "root": "10"
+//   "inOrderArray": [1, 2, 5, 5, 10, 15, 22],
+//   "postOrderArray": [1, 2, 5, 5, 22, 15, 10],
+//   "preOrderArray": [10, 5, 2, 1, 5, 15, 22]
 // }
