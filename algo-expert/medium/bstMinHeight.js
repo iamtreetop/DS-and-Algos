d8b6f3d9df27cd22b89e96a6a165bd29277ec63c
iamtreetop <trivta@gmail.com>
2021-04-02 23:30:22 -0700

    Solve MinHeight BST with naive approach

--- algo-expert/medium/bstMinHeight.js
+++ algo-expert/medium/bstMinHeight.js
@@ -1,6 +1,25 @@
 function minHeightBst(array) {
-  // Write your code here.
+	return constructMinHeightBst(array, null, 0, array.length - 1);
 }
+// sorted and distinct!
+function constructMinHeightBst(array, bst, startIdx, endIdx) {
+	// 1. find midpoint (root node)
+	// 2. recursive on left half with midpoint
+	// 3. recursive on right half with midpoint
+	if (endIdx < startIdx) return;
+	const midIdx = Math.floor((startIdx + endIdx) / 2);
+	const valueToAdd = array[midIdx];
+	if (bst === null) {
+		bst = new BST(valueToAdd);
+	} else {
+		bst.insert(valueToAdd);
+	}
+	constructMinHeightBst(array, bst, startIdx, midIdx - 1);
+	constructMinHeightBst(array, bst, midIdx + 1, endIdx);
+	return bst;
+}
+
+array = [1, 2, 5, 7, 10, 13, 14, 15, 22];
 
 class BST {
   constructor(value) {
