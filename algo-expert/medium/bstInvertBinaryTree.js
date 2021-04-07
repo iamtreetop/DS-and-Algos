4b7fa7a686263b7a39a7afeb99dbafff0910fa3e
iamtreetop <trivta@gmail.com>
2021-04-06 18:32:05 -0700

    Solve Invert Binary Tree problem iteratively

--- algo-expert/medium/bstInvertBinaryTree.js
+++ algo-expert/medium/bstInvertBinaryTree.js
@@ -1,5 +1,23 @@
+// iterative solution
+// O(n) time | O(n) space
 function invertBinaryTree(tree) {
-  // Write your code here.
+  // initialize a queue, BFS style (layer by layer)
+	// swap left node and right nodes
+	// push into queue
+	const queue = [tree];
+	while (queue.length) {
+		const currNode = queue.shift();
+		if (currNode === null) continue;
+		swapLeftAndRight(currNode);
+		queue.push(currNode.left);
+		queue.push(currNode.right);
+	}
+}
+
+function swapLeftAndRight(tree) {
+	const left = tree.left;
+	tree.left = tree.right;
+	tree.right = left;
 }
 
 // This is the class of the input binary tree.
