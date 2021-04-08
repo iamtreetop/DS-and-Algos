6c0520e6d516ff67b4c9a2b23314eb37d7ecfff3
iamtreetop <trivta@gmail.com>
2021-04-07 22:05:48 -0700

    Solve First Non-Repeating Char problem brute force

--- algo-expert/easy/firstNonRepeatingChar.js
+++ algo-expert/easy/firstNonRepeatingChar.js
@@ -1,6 +1,15 @@
+// brute force - nested loop
+// O(n^2) time | O(1) space - where n is length of string
 function firstNonRepeatingCharacter(string) {
-  // Write your code here.
-  return -1;
-};
+  for (let i = 0; i < string.length; i++) {
+		let dupeCheck = false;
+		for (let j = 0; j < string.length; j++) {
+			if (string[i] === string[j] && i !== j) dupeCheck = true;
+		}
+		
+		if (!dupeCheck) return i;
+	}
+	return -1;
+}
 
 string = "abcdcaf"; // => "b"
