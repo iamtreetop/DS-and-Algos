8bea7539aa1b71161638c9099b9df661193b9a3f
iamtreetop <trivta@gmail.com>
2021-05-24 11:34:24 -0700

    Solve leetcode 226 iteratively with DFS and BFS

--- leetcode/top-100/226.InvertBinaryTree.js
+++ leetcode/top-100/226.InvertBinaryTree.js
@@ -1,26 +1,57 @@
 // Given the root of a binary tree, invert the tree, and return its root.
 
-var invertTree = function (root) {
-  
+var invertTreeRecur = function (root) {
+  if (!root) {
+    return root;
+  }
+
+  [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
+
+  return root;
+};
+
+var invertTreeDfs = function (root) {
+  const stack = [root];
+
+  while (stack.length) {
+    const node = stack.pop();
+
+    if (node) {
+      [node.left, node.right] = [node.right, node.left];
+      stack.push(node.left, node.right);
+    }
+  }
+
+  return root;
+};
+
+var invertTreeBfs = function (root) {
+  const queue = [root];
+
+  while (queue.length) {
+    const node = queue.shift();
+
+    if (node) {
+      [node.left, node.right] = [node.right, node.left];
+      queue.push(node.left, node.right);
+    }
+  }
+
+  return root;
 };
 
 // Example 1:
-
-
 // Input: root = [4,2,7,1,3,6,9]
 // Output: [4,7,2,9,6,3,1]
+
 // Example 2:
-
-
 // Input: root = [2,1,3]
 // Output: [2,3,1]
-// Example 3:
 
+// Example 3:
 // Input: root = []
 // Output: []
- 
 
 // Constraints:
-
 // The number of nodes in the tree is in the range [0, 100].
 // -100 <= Node.val <= 100
