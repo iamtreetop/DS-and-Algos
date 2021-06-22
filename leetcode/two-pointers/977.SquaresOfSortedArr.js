de6bda224bef7426cb6030df72de3f0fe52e8d5d
iamtreetop <trivta@gmail.com>
2021-06-22 12:01:11 -0700

    Add optimized solution for leetcode 977

--- leetcode/two-pointers/977.SquaresOfSortedArr.js
+++ leetcode/two-pointers/977.SquaresOfSortedArr.js
@@ -1,6 +1,26 @@
 // Given an integer array nums sorted in non-decreasing order, return an array of the squares 
 // of each number sorted in non-decreasing order.
 
+var sortedSquaresTwoPointers = function (nums) {
+  let arr = new Array(nums.length),
+    left = 0,
+    right = nums.length - 1;
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    let leftNum = Math.abs(nums[left]),
+      rightNum = Math.abs(nums[right]);
+
+    if (leftNum < rightNum) {
+      arr[i] = Math.pow(nums[right], 2);
+      right--;
+    } else {
+      arr[i] = Math.pow(nums[left], 2);
+      left++;
+    }
+  }
+  return arr;
+};
+
 var sortedSquares = function (nums) {
   let arr = nums.map((num) => num * num);
 
