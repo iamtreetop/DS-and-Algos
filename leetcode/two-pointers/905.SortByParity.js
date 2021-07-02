ccbfc23efb14fa5c4d37d8eba3f9238d48c6d49e
iamtreetop <trivta@gmail.com>
2021-07-02 00:23:38 -0700

    Solve leetcode 905

--- leetcode/two-pointers/905.SortByParity.js
+++ leetcode/two-pointers/905.SortByParity.js
@@ -3,7 +3,34 @@
 
 // You may return any answer array that satisfies this condition.
 
- 
+var sortArrayByParity = function (nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    if (nums[left] % 2 === 1 && nums[right] % 2 === 0) {
+      swap(nums, left, right);
+      left++;
+      right--;
+    }
+
+    if (nums[left] % 2 === 0) {
+      left++;
+      continue;
+    }
+    if (nums[right] % 2 !== 0) {
+      right--;
+      continue;
+    }
+  }
+  return nums;
+};
+
+const swap = (array, i, j) => {
+  let tmp = array[i];
+  array[i] = array[j];
+  array[j] = tmp;
+};
 
 // Example 1:
 
