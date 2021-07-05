f92569ea8c5a5b0ba0ec937e9535d8dca4ede02a
iamtreetop <trivta@gmail.com>
2021-07-04 20:05:21 -0700

    Add leetcode problem 266

---
+++ leetcode/266.CanPermutatePalindrome.js
@@ -0,0 +1,15 @@
+// Given a string s, return true if a permutation of the string could form a palindrome.
+
+var canPermutePalindrome = function (s) {};
+
+// Example 1:
+// Input: s = "code"
+// Output: false
+
+// Example 2:
+// Input: s = "aab"
+// Output: true
+
+// Example 3:
+// Input: s = "carerac"
+// Output: true
