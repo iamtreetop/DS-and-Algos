8688d0db1e32f04c0e8bbfaf700396f698d46352
iamtreetop <trivta@gmail.com>
2021-07-04 20:18:06 -0700

    Solve leetcode 266

--- leetcode/266.CanPermutatePalindrome.js
+++ leetcode/266.CanPermutatePalindrome.js
@@ -1,6 +1,17 @@
 // Given a string s, return true if a permutation of the string could form a palindrome.
 
-var canPermutePalindrome = function (s) {};
+var canPermutePalindrome = function (s) {
+  const hash = {};
+  for (const char of s) {
+    if (hash[char]) {
+      delete hash[char];
+    } else {
+      hash[char] = true;
+    }
+  }
+
+  return Object.keys(hash).length <= 1;
+};
 
 // Example 1:
 // Input: s = "code"
