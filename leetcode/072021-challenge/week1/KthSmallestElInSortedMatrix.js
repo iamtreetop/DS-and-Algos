8f28b3742761d26d224cb8a3bb8447d52f619306
iamtreetop <trivta@gmail.com>
2021-07-07 11:34:52 -0700

    Add optimized solution for leetcode 070721 challengee w/ time and space comp

--- leetcode/072021-challenge/week1/KthSmallestElInSortedMatrix.js
+++ leetcode/072021-challenge/week1/KthSmallestElInSortedMatrix.js
@@ -3,11 +3,43 @@
 
 // Note that it is the kth smallest element in the sorted order, not the kth distinct element.
 
+// brute force
+// Time O(n) | space: O(n)
 var kthSmallest = function (matrix, k) {
   const flattened = matrix.flat();
   const sorted = flattened.sort((a, b) => a - b);
 
   return sorted[k - 1];
+};
+
+// bSearch
+// Time O(log(n)) | Space O(n)
+var kthSmallest = function (matrix, k) {
+  let left = matrix[0][0],
+    right = matrix[matrix.length - 1][matrix[0].length - 1];
+
+  while (left < right) {
+    let mid = left + Math.floor((right - left) / 2);
+    let count = 0;
+
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix.length; j++) {
+        if (matrix[i][j] <= mid) {
+          count++;
+        } else {
+          break;
+        }
+      }
+    }
+
+    if (count < k) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  return left;
 };
 
 // Example 1:
