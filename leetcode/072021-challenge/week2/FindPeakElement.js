8c1c91fb15369ee2e0456c3bca4125cbe46a0767
iamtreetop <trivta@gmail.com>
2021-07-13 12:45:59 -0700

    Add optimized solution for july challenge day 13

--- leetcode/072021-challenge/week2/FindPeakElement.js
+++ leetcode/072021-challenge/week2/FindPeakElement.js
@@ -7,9 +7,27 @@
 
 // You must write an algorithm that runs in O(log n) time.
 
+// BSearch Iterative
+// Time O(logn) | space O(1)
+var findPeakElement = function (nums) {
+  let left = 0,
+    right = nums.length - 1;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] > nums[mid + 1]) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return left;
+};
 
 // Brute Force
-// Time O(n)
+// Time O(n) | Space O(1)
 var findPeakElement = function (nums) {
   for (let i = 1; i < nums.length; i++) {
     if (nums[i] < nums[i - 1]) return i - 1;
